3b259eee59390b6983564475134bcb4854b19f60
ravily96-coder <ravily96@gmail.com>
2026-07-14 13:23:42 +0500

    Update ai_models.py

--- ai_models.py
+++ ai_models.py
@@ -1,1 +1,33 @@
+from PIL import Image
+import requests
+import os
+from io import BytesIO
 
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+
+class AIProvider:
+
+    def __init__(self):
+        self.token = HF_TOKEN
+
+    def enhance(self, image: Image.Image):
+        raise NotImplementedError
+
+    def restore(self, image: Image.Image):
+        raise NotImplementedError
+
+    def rejuvenate(self, image: Image.Image):
+        raise NotImplementedError
+
+
+class DemoProvider(AIProvider):
+
+    def enhance(self, image):
+        return image
+
+    def restore(self, image):
+        return image
+
+    def rejuvenate(self, image):
+        return image
